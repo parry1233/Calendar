55d6887c4020bcb75f4422e1ce12f437cb22a93e
Perry <49940223+parry1233@users.noreply.github.com>
2020-10-02 21:50:29 +0800

    Add files via upload

---
+++ NET_hw7_calender/NET_hw7_calender/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace NET_hw7_calender
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+			this.panel1 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// monthCalendar1
+			// 
+			this.monthCalendar1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.monthCalendar1.Location = new System.Drawing.Point(9, 75);
+			this.monthCalendar1.Name = "monthCalendar1";
+			this.monthCalendar1.TabIndex = 1;
+			this.monthCalendar1.TodayDate = new System.DateTime(2019, 10, 25, 0, 0, 0, 0);
+			this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pictureBox2.Location = new System.Drawing.Point(12, 18);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(454, 292);
+			this.pictureBox2.TabIndex = 3;
+			this.pictureBox2.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("新細明體", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label1.Location = new System.Drawing.Point(63, 22);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(169, 44);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Calendar";
+			// 
+			// imageList1
+			// 
+			this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+			this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+			this.imageList1.Images.SetKeyName(0, "four-seasons - 複製.jpeg");
+			this.imageList1.Images.SetKeyName(1, "four-seasons - 複製 (2).jpeg");
+			this.imageList1.Images.SetKeyName(2, "four-seasons - 複製 (3).jpeg");
+			this.imageList1.Images.SetKeyName(3, "four-seasons - 複製 (4).jpeg");
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.monthCalendar1);
+			this.panel1.Location = new System.Drawing.Point(472, 18);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(314, 298);
+			this.panel1.TabIndex = 5;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(798, 328);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.pictureBox2);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MonthCalendar monthCalendar1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ImageList imageList1;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
+
---
+++ NET_hw7_calender/NET_hw7_calender/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NET_hw7_calender
+{
+	public partial class Form1 : Form
+	{
+		private string selectedDate;
+		public Form1()
+		{
+			InitializeComponent();
+			this.selectedDate = "";
+			DateTime d = monthCalendar1.SelectionRange.End;
+			checkMonth(d.Month);
+		}
+
+		private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+		{
+			DateTime d = monthCalendar1.SelectionRange.End;
+			selectedDate = d.Year.ToString()+"-"+d.Month.ToString()+"-"+d.Day.ToString(); //Get the month selected.
+			//MessageBox.Show("Selected Date:" + selectedDate);
+			checkMonth(d.Month);
+		}
+
+		private void checkMonth(int m)
+		{
+			if(m>=3&&m<=5)
+			{
+				pictureBox2.Image = imageList1.Images[0];
+			}
+			else if(m>=6&&m<=8)
+			{
+				pictureBox2.Image = imageList1.Images[1];
+			}
+			else if(m>=9&&m<=11)
+			{
+				pictureBox2.Image = imageList1.Images[2];
+			}
+			else if(m>=12||m<=2)
+			{
+				pictureBox2.Image = imageList1.Images[3];
+			}
+			else
+			{
+				MessageBox.Show("Err: unpredicted error. Bug Existed. month="+m);
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
